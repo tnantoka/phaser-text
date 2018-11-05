f97f093fffaf3e2376f7e208625a49e5ea6e1797
tnantoka <tnantoka@bornneet.com>
2018-11-05 23:10:33 +0900

    Use const instead of var

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var config = {
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -14,7 +14,7 @@ var config = {
   }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload() {
   this.load.setBaseURL('./');
@@ -27,15 +27,15 @@ function preload() {
 function create() {
   this.add.image(400, 300, 'sky');
 
-  var particles = this.add.particles('red');
+  const particles = this.add.particles('red');
 
-  var emitter = particles.createEmitter({
+  const emitter = particles.createEmitter({
     speed: 100,
     scale: { start: 1, end: 0 },
     blendMode: 'ADD'
   });
 
-  var logo = this.physics.add.image(400, 100, 'logo');
+  const logo = this.physics.add.image(400, 100, 'logo');
 
   logo.setVelocity(100, 200);
   logo.setBounce(1, 1);
