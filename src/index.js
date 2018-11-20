435511102b67fda2c6485292fe8d26e0654349e2
tnantoka <tnantoka@bornneet.com>
2018-11-20 21:57:35 +0900

    Add text example

--- src/index.js
+++ src/index.js
@@ -17,29 +17,12 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.setBaseURL('./');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
 }
 
 function create() {
-  this.add.image(400, 300, 'sky');
+  this.add.text(10, 10, 'Text 1');
+  this.add.text(10, 40, ['Text 2-1', 'Text 2-2']);
 
-  const particles = this.add.particles('red');
-
-  const emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD'
-  });
-
-  const logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
+  this.add.text(10, 90, 'Text 3', { fontSize: '25px', fontFamily: 'fantasy' });
+  this.add.text(10, 120, 'Text 4', { backgroundColor: 'white', color: 'black' });
 }
